52dc8c767f0abf3983d21365c6d49d1ab7fc13d2
Calenaur <timhoekstra2015@gmail.com>
2020-01-30 11:46:10 +0100

    Add demo visuals

--- target/classes/static/js/scripts.js
+++ target/classes/static/js/scripts.js
@@ -3,7 +3,7 @@
 
 let count = document.getElementById('count');
 let dataset = [12, 19, 3, 5, 2, 3, 3];
-let labelset = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let labelset = ['T-60', 'T-50', 'T-40', 'T-30', 'T-20', 'T-10', 'T'];
 let linecolor = "#EF1A24";
 let list = document.getElementById('history-list');
 var ctx = document.getElementById('dailyChart').getContext('2d');
@@ -12,7 +12,7 @@ var dailyChart = new Chart(ctx, {
     data: {
         labels: labelset,
         datasets: [{
-            label: 'Temperature',
+            label: 'Heat Index Temperature in degrees °C',
             data: dataset,
             borderColor: linecolor,
             backgroundColor: "rgba(239,26,36,0.23)"
@@ -34,11 +34,18 @@ setInterval(function() {
     let number = Math.floor(Math.random() * 40) + 1;
     dataset.push(number);
     dataset.shift();
-    labelset.push(labelset[0]);
-    labelset.shift();
     dailyChart.update();
     let element = document.createElement('LI');
     let node = document.createTextNode(number);
     element.appendChild(node);
     list.appendChild(element);
 }, 5000);
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FlbDEyIiwiYSI6ImNrNjBrM3ZhNjA4NjMzb255ZHhuaXFjMmIifQ.9wOwjE4_ev_0oaczMlIB5w';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/kael12/ck60k4zxc0mvj1iqcd14jcmld', // stylesheet location
+    center: [31.55, 31.1], // starting position
+    zoom: 7
+});
