f10696ac413bf31dc5f1599da81e167bfbe65c78
Calenaur <timhoekstra2015@gmail.com>
2020-02-03 12:41:45 +0100

    Refactor webbase (#2)
    * more refactoring
    
    Removing unnecisery stuuf. and playing with some makeup.
    
    * struff
    
    * got jar extract working
    
    now able to run server from jar
    
    * Delete JWeatherApp_jar.xml
    
    * ignore
    
    * Update .gitignore
    
    * f
    
    f
    
    * api upload files
    
    * stuff
    
    * Login and css fix
    
    * some changes to login
    
    * Add demo visuals
    
    * Fix dashboard styling
    
    Co-authored-by: Akoens <43842651+Akoens@users.noreply.github.com>

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,17 @@
 package app;
 
-import app.index.*;
-import app.login.*;
-import app.user.*;
-import app.util.*;
+import app.index.IndexController;
+import app.login.LoginController;
+import app.api.ApiController;
+import app.settings.SettingsController;
+import app.signup.SignupController;
+import app.user.UserDao;
+import app.util.Filters;
+import app.util.Path;
+import app.util.ViewUtil;
 
 import static spark.Spark.*;
-import static spark.debug.DebugScreen.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Main {
 
@@ -16,19 +21,27 @@ public class Main {
 
         userDao = new UserDao();
 
-        port(8080);
-        staticFiles.location("/public");
+
+        port(4567);
+        staticFiles.location("/static");
         staticFiles.expireTime(600L);
+
         enableDebugScreen();
 
         // Set up before-filters (called before each get/post)
         before("*", Filters.addTrailingSlashes);
         before("*",Filters.handleLocaleChange);
 
+        // Get and Post
         get(Path.Web.INDEX, IndexController.serveIndexPage);
         get(Path.Web.LOGIN, LoginController.serveLoginPage);
+        get(Path.Web.SIGNUP, SignupController.serveSignupPage);
+        get(Path.Web.SETTINGS, SettingsController.serveSettingsPage);
+        get(Path.Web.API, ApiController.serveApiPage);
+
         post(Path.Web.LOGIN,LoginController.handleLoginPost);
         post(Path.Web.LOGOUT,LoginController.handleLogoutPost);
+        post(Path.Web.API, ApiController.handleApi);
         get("*", ViewUtil.notFound);
 
         //Set up after-filters (called after each get/post)
--- src/main/java/app/util/Path.java
+++ src/main/java/app/util/Path.java
@@ -4,15 +4,21 @@ import lombok.*;
 
 public class Path {
     public static class Web {
-        @Getter public static final String INDEX = "/app/index/";
-        @Getter public static final String LOGIN = "/app/login/";
+        @Getter public static final String INDEX = "/";
+        @Getter public static final String LOGIN = "/login/";
         @Getter public static final String LOGOUT = "/logout/";
+        @Getter public static final String SIGNUP = "/signup/";
+        @Getter public static final String SETTINGS = "/settings/";
+        @Getter public static final String API = "/api/";
 
     }
 
     public static class Template {
         public final static String INDEX = "/velocity/index/index.vm";
         public final static String LOGIN = "/velocity/login/login.vm";
+        public static final String SIGNUP = "/velocity/signup/signup.vm";
+        public final static String SETTINGS = "/velocity/settings/settings.vm";
+        public final static String API = "/velocity/api/api.vm";
         public static final String NOT_FOUND = "/velocity/notFound.vm";
     }
 }
