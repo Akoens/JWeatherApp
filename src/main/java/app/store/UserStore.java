d74d9d5479bc5c4df5e9a90c945d7639211fde34
Arjan <arjan@koens.biz>
2020-02-05 14:04:41 +0100

    some user update

--- src/main/java/app/store/UserStore.java
+++ src/main/java/app/store/UserStore.java
@@ -18,6 +18,7 @@ public class UserStore {
     public void createTables() throws SQLException {
         statement = connection.createStatement();
         statement.execute("CREATE TABLE IF NOT EXISTS \"Users\" (\"id\"INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\"email\" TEXT UNIQUE,\"hashed_password\" TEXT,\"auth_level\" INTEGER)");
+        statement.close();
     }
 
     /*
@@ -36,6 +37,7 @@ public class UserStore {
                     "(2, 'davidase@gmail.com', '$2a$10$e0MYzXyjpJS7Pd0RVvHwHe1HlCS4bZJ18JuywdEMLT83E1KDmUhCy', 1)," +
                     "(3, 'federico@gmail.com', '$2a$10$E3DgchtVry3qlYlzJCsyxeSK0fftK4v0ynetVCuDdxGVl1obL.ln2', 2)"
         );
+        statement.close();
     }
 
     public User getUserByEmail(String email) {
@@ -43,6 +45,7 @@ public class UserStore {
             PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Users WHERE email=?");
             pstmt.setString(1,email);
             ResultSet result = pstmt.executeQuery();
+            pstmt.close();
             return new User(result.getString("email"),result.getString("hashed_password"), result.getInt("auth_level"));
         } catch (SQLException e){
             System.err.println("GettingUserByEmail Error" + e.getMessage());
@@ -50,26 +53,49 @@ public class UserStore {
         return null;
     }
 
-    public Iterable<String> getAllEmails() throws SQLException {
-        ResultSet result = statement.executeQuery("SELECT email FROM Users");
-        ArrayList<String> emails = new ArrayList<String>();
-
-        while(result.next()){
-            emails.add(result.getString("email"));
+    public Iterable<String> getAllEmails() {
+        try {
+            statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT email FROM Users");
+            ArrayList<String> emails = new ArrayList<String>();
+            while(result.next()){
+                emails.add(result.getString("email"));
+            }
+            statement.close();
+            return emails;
+        } catch (SQLException e) {
+            System.err.println("Failed to get all emails:" + e.getMessage());
         }
-        return emails;
+        return null;
     }
 
+    //Add new users to the database
     public void addUser(String email, String hashedPassword, int authLevel){
 
+        try{
+            PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO Users(email,hashed_password,auth_level)VALUES(?,?,?) ");
+            prepareStatement.setString(1, email);
+            prepareStatement.setString(2, hashedPassword);
+            prepareStatement.setInt(3, authLevel);
+            prepareStatement.execute();
+        } catch (SQLException e){
+            System.err.println("Failed to insert:" + e.getMessage());
+        }
     }
-    public void addUsers(User user){
-
+    public void addUser(User user){
+        try{
+            PreparedStatement prepareStatement = connection.prepareStatement("INSERT INTO Users(email,hashed_password,auth_level)VALUES(?,?,?) ");
+            prepareStatement.setString(1, user.getEmail());
+            prepareStatement.setString(2, user.getHashedPassword());
+            prepareStatement.setInt(3, user.getAuthLevel());
+            prepareStatement.execute();
+        } catch (SQLException e){
+            System.err.println("Failed to insert:" + e.getMessage());
+        }
     }
 
     public void close(){
         try {
-            statement.close();
             connection.close();
         } catch (SQLException e){
             System.err.println("Connection closing Error" + e.getMessage());
